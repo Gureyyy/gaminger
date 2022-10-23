c0445d4c4eb36563406478773dddc8d0826838ba
Louis <louis.mtgn@gmail.com>
2022-10-23 17:21:18 +0200

    Sauvegarde du 23/10/22

---
+++ resources/views/events.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Gaminger</title>
+    <link rel="stylesheet" href="/gaminger/resources/css/events.css">
+
+</head>
+
+<body>
+
+    <ul class="menu">
+        <li>
+            <a href="accueil">Accueil</a>
+        </li>
+        <li>
+            <a href="cocktails">Cocktails</a>
+        </li>
+        <li>
+            <a href="events" class="actif">Événements</a>
+        </li>
+        <li>
+            <a href="contact">Contact</a>
+        </li>
+    </ul>
+    
+</body>
+
+</html>
